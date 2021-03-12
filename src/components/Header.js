38e58dc2afd58e6ddc6ef9aba1b58012570b51aa
Visvaldas <visva.rapalis@gmail.com>
2021-03-12 19:24:41 +0200

    Creating Links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 class Header extends React.Component {
   render() {
     return (
       <nav className='navbar navbar-light'>
         <div className='container'>
-          <a className='navbar-brand'>{this.props.appName.toLowerCase()}</a>
+          <Link to='/' className='navbar-brand'>
+            {this.props.appName.toLowerCase()}
+          </Link>
+
+          <ul className='nav navbar-nav pull-xs-right'>
+            <li className='nav-item'>
+              <Link to='/' className='nav-link'>
+                Home
+              </Link>
+            </li>
+
+            <li className='nav-item'>
+              <Link to='login' className='nav-link'>
+                Sign in
+              </Link>
+            </li>
+          </ul>
         </div>
       </nav>
     );
