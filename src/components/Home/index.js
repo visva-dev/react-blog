03770cca567c9bbbeb911ee566d4c6bd15099ca5
Visvaldas <visva.rapalis@gmail.com>
2021-03-12 17:45:58 +0200

    Creating Promise Middleware

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,13 +1,24 @@
 import Banner from './Banner';
 import MainView from './MainView';
 import React from 'react';
+import agent from '../../agent';
 import { connect } from 'react-redux';
+
+const Promise = global.Promise;
 
 const mapStateToProps = (state) => ({
   appName: state.appName,
 });
 
+ const mapDispatchToProps = dispatch => ({
+  onLoad: (payload) =>
+   dispatch({ type: 'HOME_PAGE_LOADED', payload }),
+ });
+
 class Home extends React.Component {
+ componentWillMount() {
+   this.props.onLoad(agent.Articles.all());
+ }
   render() {
     return (
       <div className='home-page'>
@@ -29,4 +40,4 @@ class Home extends React.Component {
   }
 }
 
-export default connect(mapStateToProps, () => ({}))(Home);
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import App from './components/App';
 import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
 import React from 'react';
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
+import { promiseMiddleware } from './middleware';
 
 const defaultState = {
   appName: 'conduit',
@@ -15,7 +16,8 @@ const reducer = function (state = defaultState, action) {
   }
   return state;
 };
-const store = createStore(reducer);
+
+const store = createStore(reducer, applyMiddleware(promiseMiddleware));
 
 ReactDOM.render(
   <Provider store={store}>
