fd0cd2ebd5c394b836b8caeedff27c5a73b13c1d
Visvaldas <visva.rapalis@gmail.com>
2021-03-12 16:59:01 +0200

    Setting up the react-redux library

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const mapStateToProps = (state) => ({
+  appName: state.appName,
+});
+
+class App extends React.Component {
+  render() {
+    return <div>{this.props.appName}</div>;
+  }
+}
+
+export default connect(mapStateToProps, () => ({}))(App);
--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
+import App from './components/App';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
 import React from 'react';
 import { createStore } from 'redux';
 
-const defaultState = { checked: false };
+const defaultState = {
+  appName: 'conduit',
+  articles: null,
+};
 const reducer = function (state = defaultState, action) {
   switch (action.type) {
     case 'TOGGLE':
@@ -12,28 +17,9 @@ const reducer = function (state = defaultState, action) {
 };
 const store = createStore(reducer);
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {};
-  }
-
-  componentWillMount() {
-    store.subscribe(() => this.setState(store.getState()));
-  }
-  render() {
-    const onClick = () => store.dispatch({ type: 'TOGGLE' });
-    return (
-      <div>
-        <h1>To-dos</h1>
-        <div>
-          Learn Redux&nbsp;
-          <input type='checkbox' checked={!!this.state.checked} onClick={onClick} />
-        </div>
-        {this.state.checked ? <h2>Done!</h2> : null}
-      </div>
-    );
-  }
-}
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
