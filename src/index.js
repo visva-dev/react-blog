4a117855dcf7be6e5b65b8ab0368bd02b853cb69
Visvaldas <visva.rapalis@gmail.com>
2021-03-12 16:41:15 +0200

    Creating a Store

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
+import { createStore } from 'redux';
+
+const defaultState = { checked: false };
+const reducer = function (state = defaultState, action) {
+  switch (action.type) {
+    case 'TOGGLE':
+      return { ...state, checked: !state.checked };
+  }
+  return state;
+};
+const store = createStore(reducer);
 
 class App extends React.Component {
   render() {
-    return (
-      <h1>Hello, World!</h1>
-    );
+    return <h1>Hello, World!</h1>;
   }
 }
 
-ReactDOM.render((
-  <App />
-), document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
